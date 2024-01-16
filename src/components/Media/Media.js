222ccdf733762a4879a7384af30cb161c459eab8
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-01-16 07:38:47 +0700

    add feature

---
+++ src/components/Media/Media.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Paricle from "../Particle";
+
+function Media () {
+    return (
+        <Container fluid className="media-section">
+            <Particle />
+            <Container>
+                
+            </Container>
+        </Container>
+    );
+}
+
+export default Media;
---
+++ src/components/Media/MediaCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Particle from "../Particle";
+
+function MediaCards () {
+    return (
+        <Card className="media-card-view">
+            
+        </Card>
+    );
+}
+
+export default MediaCards;
