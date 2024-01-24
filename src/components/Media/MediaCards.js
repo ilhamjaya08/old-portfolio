c90f325ddd474fef783afbd4f5262da17feada86
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-01-25 06:39:53 +0700

    compilee with error

--- src/components/Media/MediaCards.js
+++ src/components/Media/MediaCards.js
@@ -1,19 +1,8 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
 
 function MediaCards () {
     return (
-        <Card style={{ width: '18rem' }}>
-           <Card.Img variant="top" src="https://widgets.sociablekit.com/instagram-feed/iframe/25348299" />
-           <Card.Body>
-           <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-            </Card.Text>
-           <Button variant="primary">Go somewhere</Button>
-           </Card.Body>
-        </Card>
+        <p>halonpm s</p>>
     );
 }
 
