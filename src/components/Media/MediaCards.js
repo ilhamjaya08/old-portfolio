867418badb6ff888bdaf721e41ee819429eedc21
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-01-25 09:24:12 +0700

    fix error

--- src/components/Media/MediaCards.js
+++ src/components/Media/MediaCards.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiCanva,
+  SiFigma,
+  SiVercel,
+  SiWindows,
+  SiSublimetext,
+} from "react-icons/si";
 
-function MediaCards () {
-    return (
-        <p>halonpm s</p>>
-    );
+function MediaCards() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSublimetext />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCanva />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+    </Row>
+  );
 }
 
 export default MediaCards;
